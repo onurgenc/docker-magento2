4a7e4c48e5d3fa74bf0d7ac1452870f4c68035ec
Onur Genç <onurgenc@gmail.com>
2017-05-09 18:19:38 +0300

    files & directory permission fixed

--- magento2/entrypoint.sh
+++ magento2/entrypoint.sh
@@ -10,9 +10,6 @@ if [  ! "$(ls -A /var/www/html/magento2)" ]; then
     echo "Permission fixs";
     cd /var/www/html/magento2
 
-    chown -R www-data:www-data /var/www/html/magento2
-    find . -type d -exec chmod 700 {} \; && find . -type f -exec chmod 600 {} \;
-
     echo "Magento2 Installing...";
     php /var/www/html/magento2/bin/magento setup:install --base-url=$MAGENTO_BASE_URL \
                 --db-host=mysql --db-name=magento --db-user=root --db-password=123456 \
@@ -23,15 +20,15 @@ if [  ! "$(ls -A /var/www/html/magento2)" ]; then
 
     cp /root/.composer/auth.json /var/www/html/magento2/var/composer_home/auth.json
 
+
     composer require onurgenc/shopfinder dev-master
     composer update
-    php /var/www/html/magento2/bin/magento deploy:mode:set developer
-    php /var/www/html/magento2/bin/magento setup:static-content:deploy
+
+    
     php /var/www/html/magento2/bin/magento setup:upgrade
-    php /var/www/html/magento2/bin/magento setup:di:compile
-    php /var/www/html/magento2/bin/magento cache:clean
     php /var/www/html/magento2/bin/magento cache:flush
 
+    chown -R :www-data . && chmod -R 777 /var/www/html/magento2/var /var/www/html/magento2/pub
     echo "Magento2 successfully installed"
 
 else
